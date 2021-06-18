638e04638451c65cb7ef03bf0ca90970c5c15a3c
Welqum <zz.lilcookie.zz>
2021-06-17 19:45:00 -0700

    Added requester for all pages

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import requests, multiprocessing, json
 def downloader():
     pass    
 
+tags = "rating:explicit score:>600"
 
 page = 0
 while True:
-    req = f"https://konachan.com/post.json?page={page}&limit=100&tags=rating:explicit%20score:%3E600"
+    req = f"https://konachan.com/post.json?page={page}&limit=100&tags={tags}"
     req_text = requests.get(req).text
     parsed_req = json.loads(req_text)
-    print(json.dumps(parsed_req,indent=2))
-    exit()
+    if len(parsed_req) == 0:
+        break
+    for image in parsed_req:
+        file_url = image["jpeg_url"]
+        print(file_url)
+    page+=1
